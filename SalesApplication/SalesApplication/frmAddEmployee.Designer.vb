a9579595496fc36d2c16d2048f8c938c296a2a88
apoorvc <apoorv.c2@gmail.com>
2017-04-10 22:49:02 -0700

    new staff form

--- SalesApplication/SalesApplication/frmAddEmployee.Designer.vb
+++ SalesApplication/SalesApplication/frmAddEmployee.Designer.vb
@@ -22,8 +22,346 @@ Partial Class frmAddEmployee
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAddEmployee))
+        Me.panel2 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.button4 = New System.Windows.Forms.Button()
+        Me.button3 = New System.Windows.Forms.Button()
+        Me.lblProductNo = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtContractNo = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtCity = New System.Windows.Forms.TextBox()
+        Me.txtMI = New System.Windows.Forms.TextBox()
+        Me.txtProvince = New System.Windows.Forms.TextBox()
+        Me.txtStreet = New System.Windows.Forms.TextBox()
+        Me.txtFirstname = New System.Windows.Forms.TextBox()
+        Me.txtLastname = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panel2
+        '
+        Me.panel2.BackColor = System.Drawing.Color.Maroon
+        Me.panel2.Controls.Add(Me.PictureBox1)
+        Me.panel2.Controls.Add(Me.lblTitle)
+        Me.panel2.Location = New System.Drawing.Point(2, 1)
+        Me.panel2.Name = "panel2"
+        Me.panel2.Size = New System.Drawing.Size(668, 54)
+        Me.panel2.TabIndex = 16
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(8, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(45, 44)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(49, 14)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(92, 25)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Add Staff"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label15)
+        Me.GroupBox1.Controls.Add(Me.Label12)
+        Me.GroupBox1.Controls.Add(Me.Label11)
+        Me.GroupBox1.Controls.Add(Me.Label16)
+        Me.GroupBox1.Controls.Add(Me.Label13)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.lblProductNo)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtContractNo)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtCity)
+        Me.GroupBox1.Controls.Add(Me.txtMI)
+        Me.GroupBox1.Controls.Add(Me.txtProvince)
+        Me.GroupBox1.Controls.Add(Me.txtStreet)
+        Me.GroupBox1.Controls.Add(Me.txtFirstname)
+        Me.GroupBox1.Controls.Add(Me.txtLastname)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 61)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(637, 325)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Staff Information"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(443, 112)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(26, 12)
+        Me.Label15.TabIndex = 8
+        Me.Label15.Text = "City"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(443, 71)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(19, 12)
+        Me.Label12.TabIndex = 8
+        Me.Label12.Text = "MI"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(270, 71)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(55, 12)
+        Me.Label11.TabIndex = 8
+        Me.Label11.Text = "Firstname"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label16.Location = New System.Drawing.Point(99, 151)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(49, 12)
+        Me.Label16.TabIndex = 8
+        Me.Label16.Text = "Province"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(99, 112)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(36, 12)
+        Me.Label13.TabIndex = 8
+        Me.Label13.Text = "Street"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Verdana", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(99, 71)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(54, 12)
+        Me.Label10.TabIndex = 8
+        Me.Label10.Text = "Lastname"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.button4)
+        Me.GroupBox2.Controls.Add(Me.button3)
+        Me.GroupBox2.Location = New System.Drawing.Point(0, 235)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(635, 90)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        '
+        'button4
+        '
+        Me.button4.BackColor = System.Drawing.Color.Maroon
+        Me.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.button4.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight
+        Me.button4.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight
+        Me.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic)
+        Me.button4.ForeColor = System.Drawing.Color.White
+        Me.button4.Location = New System.Drawing.Point(443, 17)
+        Me.button4.Name = "button4"
+        Me.button4.Size = New System.Drawing.Size(75, 63)
+        Me.button4.TabIndex = 12
+        Me.button4.Text = "&Cancel"
+        Me.button4.UseVisualStyleBackColor = False
+        '
+        'button3
+        '
+        Me.button3.BackColor = System.Drawing.Color.Maroon
+        Me.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.button3.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight
+        Me.button3.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight
+        Me.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic)
+        Me.button3.ForeColor = System.Drawing.Color.White
+        Me.button3.Location = New System.Drawing.Point(101, 17)
+        Me.button3.Name = "button3"
+        Me.button3.Size = New System.Drawing.Size(75, 63)
+        Me.button3.TabIndex = 11
+        Me.button3.Text = "&Save"
+        Me.button3.UseVisualStyleBackColor = False
+        '
+        'lblProductNo
+        '
+        Me.lblProductNo.BackColor = System.Drawing.Color.White
+        Me.lblProductNo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblProductNo.Location = New System.Drawing.Point(101, 43)
+        Me.lblProductNo.Name = "lblProductNo"
+        Me.lblProductNo.Size = New System.Drawing.Size(165, 22)
+        Me.lblProductNo.TabIndex = 2
+        Me.lblProductNo.Text = " "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 130)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Address :"
+        '
+        'txtContractNo
+        '
+        Me.txtContractNo.BackColor = System.Drawing.Color.White
+        Me.txtContractNo.Location = New System.Drawing.Point(101, 195)
+        Me.txtContractNo.Name = "txtContractNo"
+        Me.txtContractNo.Size = New System.Drawing.Size(165, 21)
+        Me.txtContractNo.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 198)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Contact No. :"
+        '
+        'txtCity
+        '
+        Me.txtCity.BackColor = System.Drawing.Color.White
+        Me.txtCity.Location = New System.Drawing.Point(443, 127)
+        Me.txtCity.Name = "txtCity"
+        Me.txtCity.Size = New System.Drawing.Size(157, 21)
+        Me.txtCity.TabIndex = 5
+        '
+        'txtMI
+        '
+        Me.txtMI.BackColor = System.Drawing.Color.White
+        Me.txtMI.Location = New System.Drawing.Point(443, 86)
+        Me.txtMI.Name = "txtMI"
+        Me.txtMI.Size = New System.Drawing.Size(39, 21)
+        Me.txtMI.TabIndex = 2
+        '
+        'txtProvince
+        '
+        Me.txtProvince.BackColor = System.Drawing.Color.White
+        Me.txtProvince.Location = New System.Drawing.Point(101, 166)
+        Me.txtProvince.Name = "txtProvince"
+        Me.txtProvince.Size = New System.Drawing.Size(499, 21)
+        Me.txtProvince.TabIndex = 6
+        '
+        'txtStreet
+        '
+        Me.txtStreet.BackColor = System.Drawing.Color.White
+        Me.txtStreet.Location = New System.Drawing.Point(101, 127)
+        Me.txtStreet.Name = "txtStreet"
+        Me.txtStreet.Size = New System.Drawing.Size(336, 21)
+        Me.txtStreet.TabIndex = 3
+        '
+        'txtFirstname
+        '
+        Me.txtFirstname.BackColor = System.Drawing.Color.White
+        Me.txtFirstname.Location = New System.Drawing.Point(272, 86)
+        Me.txtFirstname.Name = "txtFirstname"
+        Me.txtFirstname.Size = New System.Drawing.Size(165, 21)
+        Me.txtFirstname.TabIndex = 1
+        '
+        'txtLastname
+        '
+        Me.txtLastname.BackColor = System.Drawing.Color.White
+        Me.txtLastname.Location = New System.Drawing.Point(101, 86)
+        Me.txtLastname.Name = "txtLastname"
+        Me.txtLastname.Size = New System.Drawing.Size(165, 21)
+        Me.txtLastname.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Name :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(16, 44)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 13)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Staff ID:"
+        '
+        'frmAddEmployee
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(670, 418)
+        Me.Controls.Add(Me.panel2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmAddEmployee"
         Me.Text = "frmAddEmployee"
+        Me.panel2.ResumeLayout(False)
+        Me.panel2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Private WithEvents panel2 As Panel
+    Private WithEvents PictureBox1 As PictureBox
+    Private WithEvents lblTitle As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Private WithEvents button4 As Button
+    Private WithEvents button3 As Button
+    Friend WithEvents lblProductNo As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtContractNo As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtCity As TextBox
+    Friend WithEvents txtMI As TextBox
+    Friend WithEvents txtProvince As TextBox
+    Friend WithEvents txtStreet As TextBox
+    Friend WithEvents txtFirstname As TextBox
+    Friend WithEvents txtLastname As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
 End Class
