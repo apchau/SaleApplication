88e60cc13f3fad234391affb1aecffba058c03b5
apoorvc <apoorv.c2@gmail.com>
2017-04-19 21:37:54 -0700

    big changes, wasn't able to commit regularly

--- SalesApplication/SalesApplication/frmAddInventory.Designer.vb
+++ SalesApplication/SalesApplication/frmAddInventory.Designer.vb
@@ -22,8 +22,247 @@ Partial Class frmAddInventory
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAddInventory))
+        Me.panel2 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.button4 = New System.Windows.Forms.Button()
+        Me.button3 = New System.Windows.Forms.Button()
+        Me.lblProductNo = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtContractNo = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtStreet = New System.Windows.Forms.TextBox()
+        Me.txtLastname = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panel2
+        '
+        Me.panel2.BackColor = System.Drawing.Color.Maroon
+        Me.panel2.Controls.Add(Me.PictureBox1)
+        Me.panel2.Controls.Add(Me.lblTitle)
+        Me.panel2.Location = New System.Drawing.Point(1, 1)
+        Me.panel2.Name = "panel2"
+        Me.panel2.Size = New System.Drawing.Size(668, 54)
+        Me.panel2.TabIndex = 16
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(8, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(45, 44)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(49, 14)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(136, 25)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Add Inventory"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.lblProductNo)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtContractNo)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtStreet)
+        Me.GroupBox1.Controls.Add(Me.txtLastname)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 61)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(354, 230)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Customer Information"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.button4)
+        Me.GroupBox2.Controls.Add(Me.button3)
+        Me.GroupBox2.Location = New System.Drawing.Point(1, 172)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(352, 58)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        '
+        'button4
+        '
+        Me.button4.BackColor = System.Drawing.Color.Maroon
+        Me.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.button4.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight
+        Me.button4.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight
+        Me.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic)
+        Me.button4.ForeColor = System.Drawing.Color.White
+        Me.button4.Location = New System.Drawing.Point(257, 17)
+        Me.button4.Name = "button4"
+        Me.button4.Size = New System.Drawing.Size(75, 29)
+        Me.button4.TabIndex = 12
+        Me.button4.Text = "&Cancel"
+        Me.button4.UseVisualStyleBackColor = False
+        '
+        'button3
+        '
+        Me.button3.BackColor = System.Drawing.Color.Maroon
+        Me.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.button3.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight
+        Me.button3.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight
+        Me.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic)
+        Me.button3.ForeColor = System.Drawing.Color.White
+        Me.button3.Location = New System.Drawing.Point(17, 17)
+        Me.button3.Name = "button3"
+        Me.button3.Size = New System.Drawing.Size(75, 29)
+        Me.button3.TabIndex = 11
+        Me.button3.Text = "&Save"
+        Me.button3.UseVisualStyleBackColor = False
+        '
+        'lblProductNo
+        '
+        Me.lblProductNo.BackColor = System.Drawing.Color.White
+        Me.lblProductNo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblProductNo.Location = New System.Drawing.Point(115, 27)
+        Me.lblProductNo.Name = "lblProductNo"
+        Me.lblProductNo.Size = New System.Drawing.Size(45, 22)
+        Me.lblProductNo.TabIndex = 2
+        Me.lblProductNo.Text = " "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 109)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Description:"
+        '
+        'txtContractNo
+        '
+        Me.txtContractNo.BackColor = System.Drawing.Color.White
+        Me.txtContractNo.Location = New System.Drawing.Point(115, 145)
+        Me.txtContractNo.Name = "txtContractNo"
+        Me.txtContractNo.Size = New System.Drawing.Size(58, 21)
+        Me.txtContractNo.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 148)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Units in Stock :"
+        '
+        'txtStreet
+        '
+        Me.txtStreet.BackColor = System.Drawing.Color.White
+        Me.txtStreet.Location = New System.Drawing.Point(115, 106)
+        Me.txtStreet.Name = "txtStreet"
+        Me.txtStreet.Size = New System.Drawing.Size(172, 21)
+        Me.txtStreet.TabIndex = 3
+        '
+        'txtLastname
+        '
+        Me.txtLastname.BackColor = System.Drawing.Color.White
+        Me.txtLastname.Location = New System.Drawing.Point(115, 70)
+        Me.txtLastname.Name = "txtLastname"
+        Me.txtLastname.Size = New System.Drawing.Size(114, 21)
+        Me.txtLastname.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Name :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(15, 28)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(79, 13)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Iventory ID:"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 300)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(379, 22)
+        Me.StatusStrip1.TabIndex = 17
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(0, 17)
+        '
+        'frmAddInventory
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(379, 322)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.panel2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmAddInventory"
         Me.Text = "frmAddInventory"
+        Me.panel2.ResumeLayout(False)
+        Me.panel2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Private WithEvents panel2 As Panel
+    Private WithEvents PictureBox1 As PictureBox
+    Private WithEvents lblTitle As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Private WithEvents button4 As Button
+    Private WithEvents button3 As Button
+    Friend WithEvents lblProductNo As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtContractNo As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtStreet As TextBox
+    Friend WithEvents txtLastname As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lblStatus As ToolStripStatusLabel
 End Class
